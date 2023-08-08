a78435de71b7c0a1ef33638bc1cc7ed0b8913ea2
Joel D <joeldasari10@gmail.com>
2023-08-08 22:46:48 +0530

    Done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import Axios from "axios";
 
 function App() {
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState("");
+  const [citynotfound, setCityNotFound] = useState(0);
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=2e8c751914ed287c5c4975f77833cf7e`;
+
+  const inputLocation = (event) => {
+    if (event.key === "Enter") {
+      Axios.get(url)
+        .then((res) => {
+          setCityNotFound(res.status);
+          setData(res.data);
+        })
+        .catch((e) => {
+          setCityNotFound(e.response.status);
+        });
+      setLocation("");
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="input">
+        <input
+          value={location}
+          onChange={(e) => setLocation(e.target.value)}
+          onKeyUp={inputLocation}
+          placeholder="Enter Location"
+          type="text"
+        />
+      </div>
+      {citynotfound === 404 ? (
+        <h1 className="errorMessage">City Not Found!</h1>
+      ) : (
+        <div className="container">
+          <div className="top">
+            <div>
+              <div className="location">
+                <p>{data.name}</p>
+              </div>
+              <div className="temp">
+                {data.main ? (
+                  <h1>{Math.floor(data.main.temp - 273.15)} °C</h1>
+                ) : null}
+              </div>
+            </div>
+            <div className="description">
+              {data.weather ? (
+                <p className="weather_type">{data.weather[0].main}</p>
+              ) : null}
+            </div>
+          </div>
+          {data.name !== undefined && (
+            <div className="bottom-box">
+              <div className="bottom">
+                <div className="feels">
+                  {data.main ? (
+                    <p className="bold">
+                      {Math.floor(data.main.feels_like - 273.15)} °C
+                    </p>
+                  ) : null}
+                  <p>Feels Like</p>
+                </div>
+                <div className="humidity">
+                  {data.main ? (
+                    <p className="bold">{data.main.humidity}%</p>
+                  ) : null}
+                  <p>Humidity</p>
+                </div>
+                <div className="wind">
+                  {data.wind ? (
+                    <p className="bold">{data.wind.speed.toFixed()} MPH</p>
+                  ) : null}
+                  <p>Wind Speed</p>
+                </div>
+              </div>
+            </div>
+          )}
+        </div>
+      )}
     </div>
   );
 }
